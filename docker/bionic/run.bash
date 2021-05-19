6977003dbd0b05ee81500824a5e3f21b63a03228
Denys Kotelovych <denys.kotelovych@gmail.com>
2021-05-20 00:22:42 +0300

    Added auxilary classes and arg parsing;

--- docker/bionic/run.bash
+++ docker/bionic/run.bash
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+
+SCRIPT_PATH=$(readlink -f "$0")
+SCRIPT_DIR_PATH=$(dirname "$SCRIPT_PATH")
+SRC_PATH=$(realpath "$SCRIPT_DIR_PATH/../../src")
+
 xhost +
 
 docker run -it \
@@ -7,6 +12,7 @@ docker run -it \
     --net=host \
     --env="DISPLAY=$DISPLAY" \
     --env="/tmp/.X11-unix:/tmp/.X11-unix" \
+    --volume="$SRC_PATH:/ompl_demo/src" \
     --runtime=nvidia \
     ompl_demo
 
