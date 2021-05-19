c536d8f1171c24cdf9abec3e0cde91ac65b78172
Denys Kotelovych <denys.kotelovych@gmail.com>
2021-05-19 22:43:09 +0300

    Fixed GUI running;

--- docker/bionic/run.bash
+++ docker/bionic/run.bash
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+xhost +
+
 docker run -it \
+    --gpus=all \
+    --net=host \
     --env="DISPLAY=$DISPLAY" \
+    --env="/tmp/.X11-unix:/tmp/.X11-unix" \
     --runtime=nvidia \
     ompl_demo
+
+xhost -
